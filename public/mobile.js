5705a10e01dc0158ffd12ed4a97775fbf4a02f14
Jaden McElvey <32574860+JadenMcElvey@users.noreply.github.com>
2020-08-26 16:08:15 -0400

    mobile.js fix

--- public/mobile.js
+++ public/mobile.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-function mobileCheck()
-{
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
-    {
-        mobile();
-    }
-    else
-    {
-        desktop();
-    }
-}
-
-function desktop()
-{
-    var desktop = document.getElementById("desktopDIV");
-    var mobile = document.getElementById("mobileDIV")
-
-    desktop.style.display = "block";
-    mobile.style.display = "none";
-    //alert("desktop");
-}
-
-function mobile()
-{
-    var desktop = document.getElementById("desktopDIV");
-    var mobile = document.getElementById("mobileDIV")
-
-    desktop.style.display = "none";
-    mobile.style.display = "block";
-    //alert("mobile");
-}
-=======
 function mobileCheck()
 {
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
@@ -71,4 +38,3 @@ function myFunction() {
       x.className = "topnav";
     }
   }
->>>>>>> f-parallax
